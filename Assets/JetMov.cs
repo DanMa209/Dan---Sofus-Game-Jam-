64dd65b8c40b961c045827738879f10eeac091e1
CleverProduction <sofusbatko@gmail.com>
2026-02-21 17:04:27 +0100

    Jetmovemnet

--- Assets/JetMov.cs
+++ Assets/JetMov.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputSystem; // vigtigt til det nye input system
 
-public class JetMov : MonoBehaviour
+public class JetMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float speed = 5f;
+    public float minY = -4f;
+    public float maxY = 4f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float vertical = 0f;
+
+        if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed)
+            vertical = 1f;
+
+        if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed)
+            vertical = -1f;
+
+        Vector3 move = new Vector3(0, vertical, 0) * speed * Time.deltaTime;
+        transform.position += move;
+
+        float clampedY = Mathf.Clamp(transform.position.y, minY, maxY);
+        transform.position = new Vector3(transform.position.x, clampedY, transform.position.z);
     }
 }
